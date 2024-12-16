31364d253e6de5277e373b660c0274d1b856a539
BoakyePrincella <boakyeprincella2003@gmail.com>
2024-12-16 14:36:09 +0000

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,88 @@
 #!/usr/bin/python3
-""" N queens """
+"""This is the nqueens problem"""
+
 import sys
 
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    exit(1)
+def check_valid(board, row, col):
+    """To check if the backtrack placement is valid
 
-if not sys.argv[1].isdigit():
-    print("N must be a number")
-    exit(1)
+    Args:
+        board (list[list[int]]): The board
+        row (int): the row
+        col (int): the column
 
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
-
-n = int(sys.argv[1])
-
-
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
-
-
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
+    Returns:
+        bool: True if valid, False otherwise
+    """
+    # Checking the left side row of the point
+    for i in range(col):
+        if board[row][i]:  # if there's attack return False
+            return False
+    # Checking the lower left diagonal
+    for i, j in zip(range(row, len(board)), range(col, -1, -1)):
+        if board[i][j]:  # if there's an attack return False
+            return False
+    # Checking the upper left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j]:  # if there's an attack return False
+            return False
+    return True  # return True when there's no attack
 
 
-solve(n)
+def solve_nqueens(board, col, solution):
+    """This solves the nqueens with recursion
+
+    Args:
+        board (list[list[int]]): The board
+        col (int): The column to start from1
+
+    Returns:
+        None
+    """
+    if col == len(board):
+        b = []
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if board[i][j]:
+                    b.append([i, j])
+        solution.append(b)
+        return
+    for i in range(len(board)):
+        if check_valid(board, i, col):
+            board[i][col] = 1
+            solve_nqueens(board, col + 1, solution)
+            board[i][col] = 0
+
+
+def main():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0 for j in range(N)] for i in range(N)]
+    solution = []
+    solve_nqueens(board, 0, solution)
+    solution.sort(key=lambda x: x[0][1])
+    for i in solution:
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
